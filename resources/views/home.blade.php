5fdbdec1794bcb81575aef686409404b84969f1f
itsdomsday <dgvcristobal@gmail.com>
2023-02-13 11:49:36 +0800

    Added comment

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
+        <!-- Sidebar -->
         <div class="col-md-3">
             <div class="card mb-3" style="background-color: #3B374A; border-color: #634BFF; color: #E0DDFA;">
                 <div class="card-header">Welcome, <b><a href="{{ route('profile', Auth::user()->id) }}" style="text-decoration: none; color: #E0DDFA;">{{ Auth::user()->name }}</a></b>!</div>
