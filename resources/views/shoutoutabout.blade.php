c8561607cb1543b31e3bafb89888f0e28f368e4e
shirone Fortuno <118256204+shironeF@users.noreply.github.com>
2023-02-14 13:20:15 +0800

    added navbar in about us page

--- resources/views/shoutoutabout.blade.php
+++ resources/views/shoutoutabout.blade.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     <title>ShoutOut</title>
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap');
@@ -232,11 +233,48 @@
         	}
             }
 
+            .navbar{
+                background-color: #151419;
+            }
+
+            .navbar a{
+                color: white;
+                font-size: 25px;
+                margin-left: 20px;
+            }
+
+            .navbar a:hover{
+                color: #634BFF;
+            }
+
+
+            .navbar a:focus{
+                color: #634BFF;
+            }
     </style>
 </head>
 <body>
+<nav class="navbar fixed-top navbar-expand-lg">
+  <a class="navbar-brand" href="#">ShoutOut</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About</span></a>
+      </li>
+    </ul>
+  </div>
+</nav>
     <section class="about">
-        <div id="about"><h1 class="reveal">About Us</h1></div>
+        <div id="about"><h1 class="reveal"></div>
         <div class="main">
             <div class="about-text reveal">
                 <p>Give people the <span>power</span> to build community</p>
