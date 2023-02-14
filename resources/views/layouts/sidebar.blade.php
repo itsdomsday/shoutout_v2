882f9f58d069cd47f69c737e5a523b83705f2d84
itsdomsday <dgvcristobal@gmail.com>
2023-02-15 00:42:57 +0800

    Fixed bugs and minor layouts

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,8 +1,8 @@
 <div class="col-md-3">
   <nav style="position: sticky;">
   <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-  <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true" style="background-color: #634BFF; color: #E0DDFA;">Home</a>
-  <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false" style="color: #E0DDFA">Profile</a>
+  <a class="nav-link" id="v-pills-home-tab" data-toggle="pill" href="{{ url('/home') }}" role="tab" aria-controls="v-pills-home" aria-selected="true" style="background-color: #634BFF; color: #E0DDFA;">Home</a>
+  <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="{{ route('profile', Auth::user()->id) }}" role="tab" aria-controls="v-pills-profile" aria-selected="false" style="color: #E0DDFA">Profile</a>
   <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false" style="color: #E0DDFA">Messages</a>
   <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false" style="color: #E0DDFA">Settings</a>
   <div>
@@ -11,7 +11,7 @@
       <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre style="color: #E0DDFA">
       My Account
       </a>
-        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+        <div class="dropdown-menu dropdown-menu-bottom" aria-labelledby="navbarDropdown">
               <a class="dropdown-item" href="{{ route('logout') }}"
                 onclick="event.preventDefault();
                 document.getElementById('logout-form').submit();">
